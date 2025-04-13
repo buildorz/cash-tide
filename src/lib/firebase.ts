a9d3b2503982b0933671032ac70b940ed7600aff
olinks <57357459+olinks@users.noreply.github.com>
2025-04-13 16:42:29 +0100

    firebase init

---
+++ src/lib/firebase.ts
@@ -0,0 +1,14 @@
+// import { getAuth,RecaptchaVerifier } from "firebase/auth";
+
+// const auth = getAuth();
+// // auth.languageCode = 'it';
+// // To apply the default browser preference instead of explicitly setting it.
+// auth.useDeviceLanguage();
+
+// window.recaptchaVerifier = new RecaptchaVerifier(auth, 'sign-in-button', {
+//     'size': 'invisible',
+//     'callback': (response) => {
+//       // reCAPTCHA solved, allow signInWithPhoneNumber.
+//       onSignInSubmit();
+//     }
+//   });
